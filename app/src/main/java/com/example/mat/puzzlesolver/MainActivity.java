82af048a0db0d8dc426fc627ac549cbd25d2b7b9
anuluj <matego99@gmail.com>
2016-12-12 18:47:34 +0100

    working camera

--- app/src/main/java/com/example/mat/puzzlesolver/MainActivity.java
+++ app/src/main/java/com/example/mat/puzzlesolver/MainActivity.java
@@ -1,13 +1,95 @@
 package com.example.mat.puzzlesolver;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2  {
+    private static final String TAG = "PuzzleSolver";
+    Mat mRgb;
+    JavaCameraView mOpenCvCameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mOpenCvCameraView = (JavaCameraView) findViewById(R.id.tutorial1_activity_java_surface_view);
+        mOpenCvCameraView.setVisibility(View.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+
+
+    }
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.i(TAG, "OpenCV Manager Connected");
+                    //from now onwards, you can use OpenCV API
+                    Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
+                    mOpenCvCameraView.enableView();
+                    break;
+                case LoaderCallbackInterface.INIT_FAILED:
+                    Log.i(TAG, "Init Failed");
+                    break;
+                case LoaderCallbackInterface.INSTALL_CANCELED:
+                    Log.i(TAG, "Install Cancelled");
+                    break;
+                case LoaderCallbackInterface.INCOMPATIBLE_MANAGER_VERSION:
+                    Log.i(TAG, "Incompatible Version");
+                    break;
+                case LoaderCallbackInterface.MARKET_ERROR:
+                    Log.i(TAG, "Market Error");
+                    break;
+                default:
+                    Log.i(TAG, "OpenCV Manager Install");
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //initialize OpenCV manager
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, mLoaderCallback);
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(mOpenCvCameraView!=null)
+            mOpenCvCameraView.disableView();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(mOpenCvCameraView!=null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgb = new Mat(width,height,CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgb.release();
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        return inputFrame.rgba();
     }
 }
